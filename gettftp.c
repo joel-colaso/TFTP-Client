906b0a722e7be96795aba39bf0c05e25631644dd
Joel Colaso <joel.colaso@ensea.fr>
2025-01-08 23:59:06 +0100

    defined RCC packet, cleaned code

--- gettftp.c
+++ gettftp.c
@@ -1,3 +1,9 @@
+/*
+ * TCB connect important  
+ * UDP does not use connect
+ *
+ */
+
 #include <stdio.h>
 #include <netdb.h>
 #include <stdio.h>
@@ -7,65 +13,33 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define N_EXPECTED_ARGUMENTS 3
+#define HOST_ARGUMENT_INDEX 1
+#define FILENAME_ARGUMENT_INDEX 2
 
 #define TFTP_PORT "69"
-
-
-/*
-
-void print_address_info(struct addrinfo *addr_list) {
-    char ipstr[INET6_ADDRLEN];
-    struct addrinfo *addr;
-    
-    for (addr = addr_list; addr != NULL; addr = addr->ai_next) {
-        void *addr_ptr;
-        
-        // Get the pointer to the address itself
-        if (addr->ai_family == AF_INET) {  // IPv4
-            struct sockaddr_in *ipv4 = (struct sockaddr_in *)addr->ai_addr;
-            addr_ptr = &(ipv4->sin_addr);
-        } else {  // IPv6
-            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)addr->ai_addr;
-            addr_ptr = &(ipv6->sin6_addr);
-        }
-        
-        // Convert IP to string
-        inet_ntop(addr->ai_family, addr_ptr, ipstr, sizeof ipstr);
-        printf("IP address: %s\n", ipstr);
-    }
-}
-
-*/
-
-
-
-
+#define RRQ_PACKET_STRING_LENGTH
+#define RRQ_PACKET_STRING_TEMPLATE "1%s\0NETASCII\0" // %s is the filename
 
 int main (int argc, char * argv[]){
 	
-	if (argc !=  3) {
+	if (argc !=  N_EXPECTED_ARGUMENTS) {
 		perror("Error: 2 arguments needed");
 	}
-	
-	
-    printf("Hello %s %s %s\n\r", argv[0], argv[1], argv[2]);
-   
-   
-    char *host = argv[1];
-    char *filename = argv[2];
+	   
+    char *host = argv[HOST_ARGUMENT_INDEX];
+    char *filename = argv[FILENAME_ARGUMENT_INDEX];
 
     printf("Host: %s\n", host);
     printf("File: %s\n", filename);
     
+
+    struct addrinfo hints = {0};
+    hints.ai_family = AF_UNSPEC;     // allow IPv4 or IPv6
+    hints.ai_socktype = SOCK_DGRAM;  // datagram socket for UDP
     
-    // Prepare hints for getaddrinfo
-    struct addrinfo hints, *server_info;
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;     // Allow IPv4 or IPv6
-    hints.ai_socktype = SOCK_DGRAM;  // Datagram socket for UDP
+    struct addrinfo *server_info;
     
-    
-    // Get address information
     int status;
     if ((status = getaddrinfo(host, TFTP_PORT, &hints, &server_info)) != 0) {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
@@ -73,17 +47,20 @@ int main (int argc, char * argv[]){
     }
     
     
-    int sock;
-	if((sock = socket (AF_INET, SOCK_DGRAM, 0)) < 0) {
+    
+    int socketFileDescriptor;
+	if((socketFileDescriptor = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		perror("socketerror") ;
 		exit(EXIT_FAILURE) ;
 	}
 
-		
+	
+	
+	char* RRQPacketString[RRQ_PACKET_STRING_LENGTH];
+	snprintf(RRQPacketString, RRQ_PACKET_STRING_TEMPLATE, filename);
     
     
     
-    // Free the linked list
     freeaddrinfo(server_info);
 
 
